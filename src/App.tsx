233d56c30991214644d9db438d1710d9d4b40f9e
DisturbedMoss <disturbedmoss35@gmail.com>
2025-10-28 19:58:46 -0300

    Implementando ListaTemas e FormTemas, ainda sem implementar a logica do DeletarTema

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import Home from './pages/home/Home'
 import Cadastro from './pages/cadastro/Cadastro'
 import Login from './pages/login/Login'
 import { AuthProvider } from './contexts/AuthContext'
+import ListaTemas from './components/tema/listaTemas/ListaTemas'
+import FormTema from './components/tema/formTema/FormTema'
+import DeletarTema from './components/tema/deletarTema/DeletarTema'
 
 function App() {
 
@@ -21,6 +24,10 @@ function App() {
                 <Route path='/' element={<Login/>}/>
                 <Route path='/cadastro' element={<Cadastro />} />
                 <Route path='/home' element={<Home />} />
+                <Route path='/temas' element={<ListaTemas/>}/>
+                <Route path='/cadastrartema' element={<FormTema />}/>
+                <Route path='/editartema/:id' element={<FormTema />}/>
+                <Route path='/deletartema/:id' element={<DeletarTema />} />
               </Routes>
 
             </div>
